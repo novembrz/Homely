2526c34c962073fddee71b6fb13a105e3280c707
Dasha <51517187+novembrz@users.noreply.github.com>
2022-10-18 18:44:17 +0500

    created project

---
+++ Homely/HomelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomelyApp.swift
+//  Homely
+//
+//  Created by Kurilova Daria Kirillovna on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
