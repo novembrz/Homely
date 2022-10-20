ef234f59856149bbce9c19e7290bb3c574921f7c
Dasha <51517187+novembrz@users.noreply.github.com>
2022-10-21 00:22:50 +0500

    added icons to project

---
+++ Homely/Homely/Extensions/Image + Extension.swift
@@ -0,0 +1,87 @@
+//
+//  Image + Extension.swift
+//  Homely
+//
+//  Created by Kurilova Daria Kirillovna on 21.10.2022.
+//
+
+import SwiftUI
+
+extension Image {
+    
+    struct Illustration {
+        static func car() -> Image {
+            return Image("car")
+        }
+        
+        static func sun() -> Image {
+            return Image("sun")
+        }
+        
+        static func lightOn() -> Image {
+            return Image("lightOn")
+        }
+        
+        static func lightOff() -> Image {
+            return Image("lightOff")
+        }
+    }
+    
+    struct Icon {
+        static func bed() -> Image {
+            return Image("bed")
+        }
+        
+        static func door() -> Image {
+            return Image("door")
+        }
+        
+        static func drop() -> Image {
+            return Image("drop")
+        }
+        
+        static func fan() -> Image {
+            return Image("fan")
+        }
+        
+        static func gamepad() -> Image {
+            return Image("gamepad")
+        }
+        
+        static func garage() -> Image {
+            return Image("garage")
+        }
+        
+        static func highway() -> Image {
+            return Image("highway")
+        }
+        
+        static func livingRoom() -> Image {
+            return Image("livingRoom")
+        }
+        
+        static func on() -> Image {
+            return Image("on")
+        }
+        
+        static func petrol() -> Image {
+            return Image("petrol")
+        }
+        
+        static func rattle() -> Image {
+            return Image("rattle")
+        }
+        
+        static func subwoofer() -> Image {
+            return Image("subwoofer")
+        }
+        
+        static func tableware() -> Image {
+            return Image("tableware")
+        }
+        
+        static func teddyBear() -> Image {
+            return Image("teddyBear")
+        }
+    }
+}
